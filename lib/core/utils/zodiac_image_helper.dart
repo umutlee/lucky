48ea3bb0b9871c9679f0fe042938cd6b7a9b8148
umutlee <umutlee@gmail.com>
2025-01-19 10:14:03 +0800

    feat: 優化生肖圖片 - 將 PNG 轉換為 JPEG 格式以減少檔案大小

--- lib/core/utils/zodiac_image_helper.dart
+++ lib/core/utils/zodiac_image_helper.dart
@@ -2,23 +2,10 @@ class ZodiacImageHelper {
   static const _basePath = 'assets/images/zodiac/Chinese 12 Zodiacs - ';
 
   static String getZodiacImage(String zodiac) {
-    final mapping = {
-      '鼠': 'Rat',
-      '牛': 'Ox',
-      '虎': 'Tiger',
-      '兔': 'Rabbit',
-      '龍': 'Dragon',
-      '蛇': 'Snake',
-      '馬': 'Horse',
-      '羊': 'Goat',
-      '猴': 'Monkey',
-      '雞': 'Rooster',
-      '狗': 'Dog',
-      '豬': 'Pig',
-    };
-
-    final englishName = mapping[zodiac] ?? 'Rat';
-    return '$_basePath$englishName.png';
+    if (!_zodiacNameMap.containsKey(zodiac)) {
+      throw ArgumentError('Invalid zodiac name: $zodiac');
+    }
+    return '$_basePath${_zodiacNameMap[zodiac]}.jpg';
   }
 
   static String getZodiacImageByYear(int year) {
@@ -29,9 +16,6 @@ class ZodiacImageHelper {
 
   // 獲取所有生肖圖片路徑
   static List<String> getAllZodiacImages() {
-    return [
-      '鼠', '牛', '虎', '兔', '龍', '蛇',
-      '馬', '羊', '猴', '雞', '狗', '豬'
-    ].map((zodiac) => getZodiacImage(zodiac)).toList();
+    return _zodiacNameMap.values.map((name) => '$_basePath$name.jpg').toList();
   }
 } 
